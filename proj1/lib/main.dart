29c949912b1d0a46679a526141c28f4cdd832e0a
Drole <drole@Droles-MacBook-Pro.local>
2020-04-19 16:04:37 +0530

    linked the first page and the login ui

--- proj1/lib/main.dart
+++ proj1/lib/main.dart
@@ -1,91 +1,55 @@
-// import 'package:flutter/material.dart';
-
-//     void main() {
-//       runApp(MaterialApp(
-//         title: 'Flutter',
-//         home: FirstScreen(),
-//       ));
-//     }
-//     class FirstScreen extends StatelessWidget {
-//       @override
-//       Widget build(BuildContext context) {
-//         return Scaffold(
-        
-//           appBar: AppBar(title: Text('Welcome to my APP'),
-//             backgroundColor: Color(0xFFFF1744)),
-//           body: Center(
-//             child: RaisedButton(
-//               child: Text(
-//                 'Click here to login',
-//                 style: TextStyle(fontSize: 24),
-
-//               ),
-//               onPressed: () {
-//                 _navigateToSecondScreen(context);
-//               },
-//             )
-//           ),
-//         );
-//       }
-
-//       void _navigateToSecondScreen(BuildContext context) {
-//         Navigator.push(
-//             context,
-//             MaterialPageRoute(
-//               builder: (context) => SecondScreen(),
-//             ));
-//       }
-//     }
-//     class SecondScreen extends StatelessWidget {
-//       @override
-//       Widget build(BuildContext context) {
-           
-//          return Scaffold (
-
-//           appBar: AppBar(title: Text("My APP")),
-
-//           body: Center(    
-//             child: Text(
-//               "Hello viewers,this is my new trial of making an app. "),
-
-
-
-
-
-
-         
-            
-//       ),  
-//     );  
-//   }  
-// }    
-            
- 
-
-        
-
-
-
 import 'package:flutter/material.dart';
- 
-void main() {
-  runApp(MaterialApp(
-    home: MyApp(),
-  ));
-}
- 
-class MyApp extends StatefulWidget {
+
+    void main() {
+      runApp(MaterialApp(
+        title: 'Flutter',
+        home: FirstScreen(),
+      ));
+    }
+    class FirstScreen extends StatelessWidget {
+      @override
+      Widget build(BuildContext context) {
+        return Scaffold(
+        
+          appBar: AppBar(title: Text('Welcome to my APP'),
+            backgroundColor: Color(0xFFFF1744)),
+          body: Center(
+            child: RaisedButton(
+              child: Text(
+                'Click here to login',
+                style: TextStyle(fontSize: 24),
+
+              ),
+              onPressed: () {
+                _navigateToSecondScreen(context);
+              },
+            )
+          ),
+        );
+      }
+
+      void _navigateToSecondScreen(BuildContext context) {
+        Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) => SecondScreen(),
+            ));
+      }
+    }
+    
+class SecondScreen extends StatefulWidget {
   @override
   _State createState() => _State();
 }
  
-class _State extends State<MyApp> {
+class _State extends State<SecondScreen> {
   TextEditingController nameController = TextEditingController();
   TextEditingController passwordController = TextEditingController();
  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+
         appBar: AppBar(
           title: Text('My App'),
         ),
@@ -166,11 +130,26 @@ class _State extends State<MyApp> {
                       )
                     ],
                     mainAxisAlignment: MainAxisAlignment.center,
-                ))
+                ),
+                  ),
+
               ],
-            )));
+            ),
+            ),
+        );
+
+       }
   }
-}
+
+
+            
+ 
+
+        
+
+
+
+
  
 
 
