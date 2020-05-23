31f983ab8c63b8d938e6529bf1e6a5ce1633c082
Drole <drole@Droles-MacBook-Pro.local>
2020-05-23 13:18:03 +0530

    lists in dart

--- test5/lib/main.dart
+++ test5/lib/main.dart
@@ -1,13 +1,11 @@
-/*void main(){
- print(add(2,3) + 4);
-}*/
-
-void main() => print(add(2,3) + 4); 
-
-/*int add(int x, int y){
-  return x + y;
-}*/
-
-int add(int x, int y) => x + y;
-
+void main(){
+  List<int> mylist = [4, 5, 7];
+   print(mylist[1]);
+  
+  
+  List<String> names = ["sanjeev", "tanvi", "Dharshini", "Hrithi"];
+  print(names[1]);
+  
+  List<dynamic> differentTypes = [true, 45, "sanjeev"];
+  print(differentTypes[0]); 
 }
