ea05324e44d35eceea0b82d3ed99bde77359802d
1developersanju <30802329+1developersanju@users.noreply.github.com>
2020-09-29 09:37:28 +0530

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,11 @@
+void main(){
+  List<int> mylist = [4, 5, 7];
+   print(mylist[1]);
+  
+  
+  List<String> names = ["sanjeev", "tanvi", "Dharshini", "Hrithi"];
+  print(names[1]);
+  
+  List<dynamic> differentTypes = [true, 45, "sanjeev"];
+  print(differentTypes[0]); 
+}
